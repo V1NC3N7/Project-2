99364e34133b89b2970d70389228f1341fc135ab
V1NC3N7 <vincent930911@gmail.com>
2019-08-19 13:25:58 +0800

    improve code logic

--- src/post_process.php
+++ src/post_process.php
@@ -6,11 +6,11 @@ if($_POST['action']=="create"){
         'Age' => $_POST['age'],
         'Gender'=> $_POST['gender']
     ));
+
 }elseif($_POST['action']=="edit"){
     $db->People->updateOne(['_id' => new \MongoDB\BSON\ObjectID($_POST['id'])],
     ['$set' => ['Name' => $_POST['name'], 'Age' => $_POST['age'], 'Gender' => $_POST['gender'],]]
 );
 }
 
-
-    header("location: index.php");
+header("location: index.php"); 
