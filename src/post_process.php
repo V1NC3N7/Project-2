3252c48e0e45c2eabb108c9a02c47e25e4d79258
V1NC3N7 <vincent930911@gmail.com>
2019-08-19 10:57:30 +0800

    include with update module

--- src/post_process.php
+++ src/post_process.php
@@ -1,11 +1,16 @@
 <?php
 include "connectDB.php";
+if($_POST['action']=="create"){
+    $db->People->insertOne(array(
+        'Name'=> $_POST['name'],
+        'Age' => $_POST['age'],
+        'Gender'=> $_POST['gender']
+    ));
+}elseif($_POST['action']=="edit"){
+    $db->People->updateOne(['_id' => new \MongoDB\BSON\ObjectID($_POST['id'])],
+    ['$set' => ['Name' => $_POST['name'], 'Age' => $_POST['age'], 'Gender' => $_POST['gender'],]]
+);
+}
 
-$db->People->insertOne(array(
-    'Name'=> $_POST['name'],
-    'Age' => $_POST['age'],
-    'Gender'=> $_POST['gender']
-    
-));
 
     header("location: index.php");
